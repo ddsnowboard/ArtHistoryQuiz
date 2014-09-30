3adc0360719ed0048e3edd40e2af19465965eeb8
ddsnowboard <ddsnowboard@gmail.com>
2014-09-30 10:48:48 -0500

    Started randomization features.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 	var current = 0;
 	var side = "front";
+	// This is the start of the randomization feature. 
+	var MASTER_CARDS = cards;
 	// Note: I defined the variable "cards" as an array of objects with 2 attributes each, front and back. I make them programmatically
 	// with a python script, so they're saved in a separate file, cards.js. 
 	$("#left").click(function() {
