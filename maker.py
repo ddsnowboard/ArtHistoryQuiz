9136f254d66e793edb4671c74e45d377d8a9e810
ddsnowboard <ddsnowboard@gmail.com>
2014-10-24 10:52:41 -0500

    Started on many cards system.

--- maker.py
+++ maker.py
@@ -3,22 +3,28 @@
 # accordingly. 
 import re
 with open("MASTER.txt", 'r') as m:
-	with open("cards.js", 'w') as o:
-		o.write("var cards = [")
-		picture = True
-		line = 0
-		for i in m:
-			line += 1
-			if picture and i == '\n':
-				raise Exception("You need a picture at line %d" % line)
-			elif re.search(r"^[/]{2}", i):
-				pass			
-			elif picture:
-				o.write("{front: '<img src=\"pictures/%s\" />', prefetch: '<img class=\"prefetch\" src=\"pictures/%s\" />', back: '" % (i.replace('\n', ''), i.replace('\n', '')))
-				picture = False
-			elif i == '\n' and not picture:
-				o.write("'}, ")
-				picture = True
-			else:
-				o.write(i.replace('\n', '<br/>').replace("'", r"\'"))
-		o.write("'}];")
+	with open("old.js", 'w') as o:
+		with open("new.js", 'w') as n:
+			which = o # Switch everything to this. 
+			o.write("var old = [")
+			n.write("var new = [")
+			picture = True
+			line = 0
+			for i in m:
+				line += 1
+				if picture and i == '\n':
+					raise Exception("You need a picture at line %d" % line)
+				elif i[0] == "~":
+					which = n
+				elif re.search(r"^[/]{2}", i):
+					pass			
+				elif picture:
+					o.write("{front: '<img src=\"pictures/%s\" />', prefetch: '<img class=\"prefetch\" src=\"pictures/%s\" />', back: '" % (i.replace('\n', ''), i.replace('\n', '')))
+					picture = False
+				elif i == '\n' and not picture:
+					o.write("'}, ")
+					picture = True
+				else:
+					o.write(i.replace('\n', '<br/>').replace("'", r"\'"))
+			o.write("'}];")
+			n.write("'}];")
