deddc11f5cc4274ae8a60d14f087e113e8bfd715
ddsnowboard <ddsnowboard@gmail.com>
2014-10-24 16:15:18 -0500

    Finished adding new functionality.
    I still need to edit the comments and readme, but I can do that later.
    Anyway, what I did was I made it so that you can only study new cards,
    old cards, or all of them, because she just told us that there would
    be none of the old pieces on the exam.

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// This is the script that governs the switching of the cards on the page. 
-// Don't change this unless you know what you're doing and want to add something. 
+// This is the script that governs the switching of the cards on the page.
+// Don't change this unless you know what you're doing and want to add something.
 $(document).ready(function () {
-	// The current card being shown. Starts at 0 (arrays are zero-indexed). 
+	// The current card being shown. Starts at 0 (arrays are zero-indexed).
 	var current = 0;
-	cards = old.concat(after);
-	// What side we're on. Either "front" or "back". Anything else will throw an error. This is case-sensitive. 
+	cards = old.concat(after).slice();
+	current_set = "all";
+	// What side we're on. Either "front" or "back". Anything else will throw an error. This is case-sensitive.
 	var side = "front";
 	// I defined the variable "cards" as an array of objects with 2 attributes each, front and back. I make them programmatically
-	// with a python script, so they're saved in a separate file, cards.js. That's why they're not here and your editor might yell at you for 
-	// talking about them. 
-	// Initializes the first card on screen. 
+	// with a python script, so they're saved in a separate file, cards.js. That's why they're not here and your editor might yell at you for
+	// talking about them.
+	// Initializes the first card on screen.
 	draw(current, side);
-	$("#left").click(function() {
-		// I need this if/else to cover wrapping around to the end if you go left from card 0. 
+	$("#left").click(function () {
+		// I need this if/else to cover wrapping around to the end if you go left from card 0.
 		if (current !== 0)
 			current--;
-		else 
+		else
 			current = cards.length - 1;
-		// Always show the front side of a card when you switch to it. 
+		// Always show the front side of a card when you switch to it.
 		side = 'front';
-		// This actually puts the stuff you just did on the page, lest it just be in this script but invisible to the user. 
+		// This actually puts the stuff you just did on the page, lest it just be in this script but invisible to the user.
 		draw(current, side);
 	});
-	// See above. 
-	$("#right").click(function(){
+	// See above.
+	$("#right").click(function () {
 		if (current !== cards.length - 1)
 			current++;
 		else
@@ -31,81 +32,100 @@ $(document).ready(function () {
 		side = 'front';
 		draw(current, side);
 	});
-	// Flips the card. 
-	$("#center").click(function() {
+	// Flips the card.
+	$("#center").click(function () {
 		if (side === 'front')
 			side = "back";
 		else
 			side = "front";
 		draw(current, side);
 	});
-	// This function governs everything that happens when some key is pressed. 
-	$(document).keydown(function(event)
-	{
-		// Key 32 is space; if you press it, flip the card (i.e. do what would be done if you clicked the center. 
-		if(event.which == 32)
+	// This function governs everything that happens when some key is pressed.
+	$(document).keydown(function (event) {
+		// Key 32 is space; if you press it, flip the card (i.e. do what would be done if you clicked the center.
+		if (event.which == 32)
 			$("#center").click();
-		// Key 39 is right. 
-		else if(event.which == 39)
+		// Key 39 is right.
+		else if (event.which == 39)
 			$("#right").click();
-		// Key 37 is left. 
-		else if(event.which == 37)
+		// Key 37 is left.
+		else if (event.which == 37)
 			$("#left").click();
 	});
-	// This shuffles the cards. 
-	$("#random").click(function() {
+	// This shuffles the cards.
+	$("#random").click(function () {
 		cards = shuffle(cards);
 		current = 0;
 		side = "front";
 		draw(current, side);
 	});
-	// I couldn't get the functionality to go get a master list and re-set it to be the actual list to run right, so this just refreshes the page. 
-	$("#unshuffle").click(function() {
-		location.reload();
+	// I couldn't get the functionality to go get a master list and re-set it to be the actual list to run right, so this just refreshes the page.
+	$("#unshuffle").click(function () {
+		switch(current_set){
+			case "old":
+				cards = old.slice();
+				break;
+			case "new":
+				cards = after.slice();
+				break;
+			case "all":
+				cards = old.concat(after);
+				break;
+		}
+		current = 0;
+		side = 'front';
+		draw(current, side);
 	});
-	$("#old").click(function() {
-		cards = old;
+	$("#old").click(function () {
+		cards = old.slice();
+		current_set = "old";
 		side = 'front';
 		current = 0;
 		draw(current, side);
 	});
-	$("#new").click(function() {
-	cards = after;
-	side = 'front';
-	current = 0;
-	draw(current, side);
-	});
-	$("#all").click(function() {
-		cards = after.concat(old);
-		side="front";
+	$("#new").click(function () {
+		cards = after.slice();
+		current_set = "new";
+		side = 'front';
 		current = 0;
 		draw(current, side);
 	});
-	// Prefetches all the images so they don't have to be loaded in real time. 
-	for(var i = 0; i<cards.length; i++)
-	{
+	$("#all").click(function () {
+		cards = old.concat(after).slice();
+		current_set = "all";
+		side = "front";
+		current = 0;
+		draw(current, side);
+	});
+	$("#hider").click(function() {
+		$("#information").slideUp();
+	});
+	// Prefetches all the images so they don't have to be loaded in real time.
+	for (var i = 0; i < cards.length; i++) {
 		$("#pre").append(cards[i.toString()]['prefetch'])
 	}
 });
-// Puts the current setup on the screen. 
+// Puts the current setup on the screen.
 function draw(current, side) {
 	$("#card").html(cards[current][side]);
-	$("#counter").html((current+1).toString()+' of '+cards.length.toString());
+	$("#counter").html((current + 1).toString() + ' of ' + cards.length.toString());
 }
 
 // Borrowed from https://stackoverflow.com/a/6274398/2570117
 function shuffle(array) {
-    var counter = array.length, temp, index;
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
-        // Decrease counter by 1
-        counter--;
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-    return array;
+	var counter = array.length,
+	temp,
+	index;
+	// While there are elements in the array
+	while (counter > 0) {
+		// Pick a random index
+		index = Math.floor(Math.random() * counter);
+		// Decrease counter by 1
+		counter--;
+		// And swap the last element with it
+		temp = array[counter];
+		array[counter] = array[index];
+		array[index] = temp;
+	}
+	return array;
 }
