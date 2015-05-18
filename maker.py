42ed253ac79660858314de165008fefb8dac5545
ddsnowboard <ddsnowboard@gmail.com>
2015-05-18 00:07:07 -0500

    Add credit for design.
    Add a credit for Ian for the new design.
    Reformat tags in maker.py. They weren't the same or right, so I made them so.
    Remove butter copy.jpg. I'm sure there's a reason it was there, but I couldn't figure
    it out.

--- maker.py
+++ maker.py
@@ -1,11 +1,11 @@
 # This is the python script that converts the human-readable text in MASTER.txt into computer-readable text in old.js and new.js.
 # There's no reason to edit this either; just edit MASTER.txt, then whenever you're done, run this and it will edit old.js and new.js
-# accordingly. 
+# accordingly.
 import re
 with open("MASTER.txt", 'r') as m:
 	with open("old.js", 'w') as o:
 		with open("new.js", 'w') as n:
-			which = o # Switch everything to this. 
+			which = o # Switch everything to this.
 			o.write("var old = [")
 			n.write("var after = [")
 			picture = True
@@ -18,7 +18,7 @@ with open("MASTER.txt", 'r') as m:
 					which = n
 					picture = True
 				elif re.search(r"^[/]{2}", i):
-					pass			
+					pass
 				elif picture:
 					which.write("{front: '<img src=\"pictures/%s\" />', prefetch: '<img class=\"prefetch\" src=\"pictures/%s\" />', back: '" % (i.replace('\n', ''), i.replace('\n', '')))
 					picture = False
@@ -26,6 +26,6 @@ with open("MASTER.txt", 'r') as m:
 					which.write("'}, ")
 					picture = True
 				else:
-					which.write(i.replace('\n', '<br/><br>').replace("'", r"\'"))
+					which.write(i.replace('\n', '<br /><br />').replace("'", r"\'"))
 			o.write("'}];")
 			n.write("'}];")
